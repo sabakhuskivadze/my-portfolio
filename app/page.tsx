691bfd6e9f73389a4ed15727e0fa4cf1bfddf183
Saba khuskivadze <sabameshveliani2008@icloud.com>
2024-02-01 21:58:33 +0400

    portolio

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Header from './header/page'
+import Main from './main/page'
+
+export default function Home() {
+  return (
+    <>
+    <Header></Header>
+    <Main></Main>
+    </>
+  )
+}
